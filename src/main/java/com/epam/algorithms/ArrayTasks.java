c27e4ba638976d92fbef7cb42b013e814a355529
Husayn Ganiev <hg222ii@student.lnu.se>
2023-01-10 16:01:17 +0100

    first commit

--- src/main/java/com/epam/algorithms/ArrayTasks.java
+++ src/main/java/com/epam/algorithms/ArrayTasks.java
@@ -11,7 +11,12 @@ public class ArrayTasks {
      * Return a String[] array that will list all the seasons of the year, starting with winter.
      */
     public String[] seasonsArray() {
-        return null;
+        String[] strings = new String[4];
+        strings[0]=("Winter");
+        strings[1]=("Spring");
+        strings[2]=("Summer");
+        strings[3]=("Autumn");
+        return  strings;
     }
 
     /**
@@ -23,7 +28,12 @@ public class ArrayTasks {
      * length = 1  -> [1] length = 3  -> [1, 2, 3] length = 5  -> [1, 2, 3, 4, 5]
      */
     public int[] generateNumbers(int length) {
-        return null;
+        int[] array = new int[length];
+        for(int i = 0; i < length; i++){
+            array[i] = 1;
+            array[i] = 1+i;
+        }
+        return array;
     }
 
     /**
@@ -34,7 +44,11 @@ public class ArrayTasks {
      * arr = [1, 3, 5]   -> sum = 9 arr = [5, -3, -4] -> sum = -2
      */
     public int totalSum(int[] arr) {
-        return 0;
+        int sum = 0;
+        for(int i=0; i < arr.length; i++){
+            sum = sum +  arr[i];
+        }
+        return sum;
     }
 
     /**
@@ -46,7 +60,11 @@ public class ArrayTasks {
      * arr = [99, -7, 102], number = -7    ->   2 arr = [5, -3, -4],   number = 10    ->  -1
      */
     public int findIndexOfNumber(int[] arr, int number) {
-        return 0;
+        for(int i = 0; i < arr.length; i++)
+            if(arr[i] == number)
+                return i;
+
+        return -1;
     }
 
     /**
@@ -58,7 +76,12 @@ public class ArrayTasks {
      * "pineapple"]
      */
     public String[] reverseArray(String[] arr) {
-        return null;
+        String[] reverse = new String[arr.length];
+        for(int i=0; i<reverse.length; i++) {
+            reverse[i] = arr[reverse.length-1-i]; //-1 bcs of how java counts array indexes
+            //System.out.print(reverse[i]);
+        }
+        return reverse;
     }
 
     /**
@@ -70,7 +93,12 @@ public class ArrayTasks {
      * arr = [1,-2, 3]      -> [1, 3] arr = [-1, -2, -3]   -> [] arr = [1, 2]         -> [1, 2]
      */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-        return null;
+        int[] positivesOnly = new int[arr.length];
+        for(int i = 0; i < arr.length; i++){
+            if ( arr[i] > 0)
+                positivesOnly[i] = arr[i];
+        }
+        return positivesOnly;
     }
 
     /**
@@ -83,6 +111,8 @@ public class ArrayTasks {
      * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]] arr = [[5, 4], [7]]       -> [[7], [4, 5]]
      */
     public int[][] sortRaggedArray(int[][] arr) {
+        // COULD NOT DO IT WITHOUT java.util... :(
+
         return null;
     }
 
